aa66b84572a7c8c88a821fd341c524c7d51fd21f
Hector Garza <hectorg@smartxchain.com>
2024-12-14 21:29:45 -0500

    feat: add quality to model

--- mro/specifications/admin.py
+++ mro/specifications/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Spec
+from .models import Spec, Quality
 
 
 @admin.register(Spec)
@@ -7,7 +7,13 @@ class SpecAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'status', 'publish']
     list_filter = ['status', 'created', 'publish', 'author']
     search_fields = ['title', 'description']
-    prepopulated_field = {'slug': ('title',)}
+    prepopulated_fields = {'slug': ('title',)}
     date_hierarchy = 'publish'
     ordering = ['status', 'publish']
     show_facets = admin.ShowFacets.ALWAYS
+
+
+@admin.register(Quality)
+class QualityAdmin(admin.ModelAdmin):
+    list_display = ['spec', 'name', 'test_spec', 'description', 'status', 'publish']
+    prepopulated_fields = {'slug': ('name',)}
--- mro/specifications/models.py
+++ mro/specifications/models.py
@@ -9,11 +9,13 @@ class PublishedManager(models.Manager):
             super().get_queryset().filter(status=Spec.Status.PUBLISHED)
         )
 
-class Spec(models.Model):
-    class Status(models.TextChoices):
+
+class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
 
+
+class Spec(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     revision = models.CharField(max_length=50)
@@ -43,3 +45,54 @@ class Spec(models.Model):
 
     def __str__(self):
         return self.title
+
+class Quality(models.Model):
+    class Method(models.TextChoices):
+        ACCEPTANCE_TEST = 'LI', 'Lot Inspection'
+        PERIODIC_TEST = 'PT', 'Periodic Test'
+        PREPRODUCTION_TEST = 'PRE', 'Preproduction Test'
+
+    class Sample(models.TextChoices):
+        PART = 'P', 'Part'
+        PANEL = 'PL', 'Panel'
+
+    spec = models.ForeignKey(
+        Spec,
+        on_delete=models.CASCADE,
+        related_name='spec_quality'
+    )
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+    methods = models.CharField(
+        max_length=3,
+        choices=Method,
+    )
+    sample = models.CharField(
+        max_length=3,
+        choices=Sample,
+        default=Sample.PANEL
+    )
+    test_spec = models.CharField(max_length=50)
+    description = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status,
+        default=Status.DRAFT
+    )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='quality_publish'
+    )
+    objects = models.Manager()
+    published = PublishedManager()
+
+    class Meta:
+        ordering = ['name']
+        indexes = [models.Index(fields=['name']),]
+        
+    def __str__(self):
+        return self.name
