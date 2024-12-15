66b4c94358dbfc5f7637ef726c2e8daec5362e8e
Hector Garza <hector.garza@piedmontaviation.com>
2024-12-15 13:45:56 -0500

    fix: classes and tanks

--- mro/specifications/admin.py
+++ mro/specifications/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Spec, Quality, Requirement, Classification
+from .models import Spec, Quality, Classification, Manual, Solution, ProcessRequired
 
 
 @admin.register(Spec)
 class SpecAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author', 'status', 'publish']
+    list_display = ['title', 'description', 'status', 'publish']
     list_filter = ['status', 'created', 'publish', 'author']
     search_fields = ['title', 'description']
     prepopulated_fields = {'slug': ('title',)}
@@ -12,16 +12,11 @@ class SpecAdmin(admin.ModelAdmin):
     ordering = ['status', 'publish']
     show_facets = admin.ShowFacets.ALWAYS
 
-
-@admin.register(Quality)
-class QualityAdmin(admin.ModelAdmin):
-    list_display = ['spec', 'name', 'test_spec', 'description', 'status', 'publish']
-    prepopulated_fields = {'slug': ('name',)}
-
-@admin.register(Requirement)
-class RequirementAdmin(admin.ModelAdmin):
-    list_display = ['step', 'title', 'process', 'description', 'status', 'publish']
-
 @admin.register(Classification)
 class ClassificationAdmin(admin.ModelAdmin):
-    list_display = ['spec', 'method', 'types', 'classes', 'status', 'publish']
+    list_display = ['spec', 'classification', 'status', 'publish']
+
+
+@admin.register(ProcessRequired)
+class ProcessRequiredAdmin(admin.ModelAdmin):
+    list_display = ['classification', 'process_identity', 'step', 'title', 'status', 'publish']
--- mro/specifications/models.py
+++ mro/specifications/models.py
@@ -42,6 +42,7 @@ class Spec(models.Model):
         indexes = [
             models.Index(fields=['-title']),
         ]
+        verbose_name_plural = 'specification'
 
     def __str__(self):
         return self.title
@@ -53,9 +54,8 @@ class Classification(models.Model):
         on_delete=models.CASCADE,
         related_name='spec_classification'
     )
-    method = models.CharField(max_length=50, default='N/A')
-    types = models.CharField(max_length=50, default='N/A')
-    classes = models.CharField(max_length=50, default='N/A')
+    classification = models.CharField(max_length=200, default='N/A', help_text='Combination of methods, types, and classes. Use only one per process')
+    description = models.TextField(blank=True, help_text='description of method, type, or class')
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -75,9 +75,11 @@ class Classification(models.Model):
     class Meta:
         ordering = ['spec']
         indexes = [models.Index(fields=['spec']),]
+        verbose_name_plural = 'classification'
         
     def __str__(self):
-        return self.spec.title
+        return f"{self.spec.title} - ({self.classification})"
+
 
 class Quality(models.Model):
     class Method(models.TextChoices):
@@ -126,29 +128,36 @@ class Quality(models.Model):
     class Meta:
         ordering = ['name']
         indexes = [models.Index(fields=['name']),]
+        verbose_name_plural = 'quality requirements'
         
     def __str__(self):
         return self.name
 
-
-class Requirement(models.Model):
-    class Require(models.TextChoices):
-        STRESS_RELIEF_TREATMENT = 'SRT', 'Stress Relief Treatment'
-        CLEANING = 'CL', 'Cleaning'
-        MAIN_PROCEDURE = 'MP', 'Procedure'
-        POST_TREATMENT = 'PT', 'Post Treatment'
+    
+class ProcessRequired(models.Model):
+    class Identity(models.TextChoices):
+        CHROME = 'CR', 'Chrome Plate'
+        NICKEL = 'NI', 'Nickel Plate'
+        CADMIUM = 'CD', 'Cadmium Plate'
+        ANODIZE = 'AN', 'Anodize'
+        CONVISION = 'CF', 'Chemical Converision'
+        PASSIVATION = 'PA', 'Passivation'
+        STRIP = 'ST', 'Coating Strip'
+        BRUSH = 'BR', 'Brush Plate'
+        PREPEN = 'PR', 'Prepen Etch'
+        TEMPERETCH = 'TE', 'Temper Etch'
 
     classification = models.ForeignKey(
         Classification,
         on_delete=models.CASCADE,
-        related_name='spec_requirement'
+        related_name='process_class'
     )
     step = models.IntegerField(default=0)
     title = models.CharField(max_length=50)
-    process = models.CharField(
+    process_identity = models.CharField(
         max_length=50,
-        choices=Require,
-        default=Require.MAIN_PROCEDURE
+        choices=Identity,
+        default='N/A'
     )
     description = models.TextField()
 
@@ -163,7 +172,7 @@ class Requirement(models.Model):
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        related_name='requirement_publish'
+        related_name='process_publish'
     )
     objects = models.Manager()
     published = PublishedManager()
@@ -171,8 +180,72 @@ class Requirement(models.Model):
     class Meta:
         ordering = ['title']
         indexes = [models.Index(fields=['title']),]
+        verbose_name_plural = 'Process Method'
         
     def __str__(self):
-        return self.title
-
+        return self.title   
     
+
+class Solution(models.Model):
+    tank_identification = models.CharField(max_length=50)
+    tank_contents = models.CharField(max_length=200, help_text='Chemical Contents')
+    temp_range = models.CharField(max_length=100, default='Ambient', help_text="Temperature in F")
+    time_min = models.CharField(max_length=50, help_text="Mininum Soak Time (minutes)")
+    time_max = models.CharField(max_length=50, help_text="Maximum Soak Time (minutes)")
+    tank_requirement = models.TextField()
+    is_rectified = models.BooleanField(default=False)
+
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status,
+        default=Status.DRAFT
+    )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='tank_publish'
+    )
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+    class Meta:
+        ordering = ['tank_identification']
+        indexes = [models.Index(fields=['tank_identification']),]
+        verbose_name_plural = 'Tank Process'
+        
+    def __str__(self):
+        return self.tank_identification
+
+
+class Manual(models.Model):
+    manual_title = models.CharField(max_length=50)
+    manual_description = models.TextField(help_text='Small description of task to be performed')
+
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status,
+        default=Status.DRAFT
+    )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='manual_publish'
+    )
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+    class Meta:
+        ordering = ['manual_title']
+        indexes = [models.Index(fields=['manual_title']),]
+        verbose_name_plural = 'Manual Process'
+        
+    def __str__(self):
+        return self.manual_title
